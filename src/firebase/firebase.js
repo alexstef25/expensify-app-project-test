544b56edf4b7b920f7de5b19e334129818ab5968
Alexandru Stefanescu <alexandru.stefanescu@mysp.com>
2019-01-20 21:06:38 +0200

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase';
 
+
 const config = {
     apiKey: "AIzaSyCbEA3NqNR8a60TJ8IpmeLvI_jkp5wMelI",
     authDomain: "expensify-c8a8a.firebaseapp.com",
@@ -12,8 +13,9 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // // child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
